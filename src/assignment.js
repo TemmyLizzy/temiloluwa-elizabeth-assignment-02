29de1ada2b63daafe6ed5c0ce4240095db304e9c
Sangobiyi temiloluwa temiloluwa <72400296+TemmyLizzy@users.noreply.github.com>
2022-07-04 21:54:37 +0000

    worked on challenge-1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  sum = 0;
+  for(number of arrayOfNumbers){
+    sum += number
+  }
+    return sum;
+    // return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
